ab96d4f4d943285b23586f8a763a6bc3bfd5b596
DybaTubeYT1 <80713813+DybaTubeYT1@users.noreply.github.com>
2022-05-21 22:22:23 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,32 @@
 
 const density = "Ñ@#W$9876543210?!abc;:+=-,._      ";
 
+const videoPaths = ['BadApple.mp4', 'cool.mp4', 'bruh.mp4']
 
-let video;
-let asciiDiv;
-let playing = true;
+var video = createVideo('BadApple.mp4')
+
+let asciiDiv
+
 
 function setup() {
   noCanvas()
 
-  video = createVideo('badapple.mp4');
-  video.size(64, 48);
-  video.volume(1)
+
+
+  video.play()
+  video.size(48,36)
+  video.hide()
   video.loop()
+  video.volume(1)
+
 
   asciiDiv = createDiv();
 }
 
+
+
 function draw() {
-  video.loadPixels();
+  video.loadPixels()
   let asciiImage = "";
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
@@ -27,7 +35,6 @@ function draw() {
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
-      const len = density.length;
       const charIndex = floor(map(avg, 0, 255, 0, 29));
       const c = density.charAt(charIndex);
       if (c == ' ') asciiImage += "&nbsp;";
