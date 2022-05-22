3f0bbd5d167270bc7eccf152c05e172c10e74558
DybaTubeYT1 <80713813+DybaTubeYT1@users.noreply.github.com>
2022-05-21 22:16:13 -0300

    UPDATE AAAAAAAAAAAAAAAAAAA

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+const density = "Ñ@#W$9876543210?!abc;:+=-,._      ";
+
+
+let video;
+let asciiDiv;
+let playing = true;
+
+function setup() {
+  noCanvas()
+
+  video = createVideo('badapple.mp4');
+  video.size(64, 48);
+  video.volume(1)
+  video.loop()
+
+  asciiDiv = createDiv();
+}
+
+function draw() {
+  video.loadPixels();
+  let asciiImage = "";
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, 0, 29));
+      const c = density.charAt(charIndex);
+      if (c == ' ') asciiImage += "&nbsp;";
+      else asciiImage += c;
+    }
+    asciiImage += '<br/>';
+  }
+  asciiDiv.html(asciiImage);
+}
