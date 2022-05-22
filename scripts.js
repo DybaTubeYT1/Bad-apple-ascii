f7f1f8999b1785225b6980328a39852ca73a7778
DybaTubeYT1 <80713813+DybaTubeYT1@users.noreply.github.com>
2022-05-21 21:51:02 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,43 +1,53 @@
 
-const density = "Ñ@#W$9876543210?!abc;:+=-,._         ";
+const density = "Ñ@#W$9876543210?!abc;:+=-,._      ";
 
+const videoPaths = ['BadApple.mp4', 'cool.mp4', 'bruh.mp4']
 
-let video;
-let asciiDiv;
-let playing = true;
+var videos
+let asciiDiv
+
 
 function setup() {
-  noCanvas();
+  noCanvas()
 
-  video = createVideo('badapple.mp4', vidLoad);
-  video.size(48,36);
+  const loadVideos = (i = -1) => {
+    i ++;
+    if (i <= videoPaths.length - 1){
+          videos[i] = createVideo(videoPaths[i], loadVideo(i));
+   } else {
+          videos[i] = createVideo(videoPaths[i]);
+   }
+   
+  }
 
+
+  videos = createVideo('badapple.mp4')
+  videos.size(48,36)
+  videos.hide()
+  videos.loop()
+  videos.volume(1)
 
   asciiDiv = createDiv();
 }
 
-function vidLoad() {
-  video.loop();
-  video.volume(1)
-}
+
 
 function draw() {
-  video.loadPixels()
+  videos.loadPixels()
   let asciiImage = "";
-  for (let j = 0; j < video.height; j++) {
-    for (let i = 0; i < video.width; i++) {
-      const pixelIndex = (i + j * video.width) * 4;
-      const r = video.pixels[pixelIndex + 0];
-      const g = video.pixels[pixelIndex + 1];
-      const b = video.pixels[pixelIndex + 2];
+  for (let j = 0; j < videos.height; j++) {
+    for (let i = 0; i < videos.width; i++) {
+      const pixelIndex = (i + j * videos.width) * 4;
+      const r = videos.pixels[pixelIndex + 0];
+      const g = videos.pixels[pixelIndex + 1];
+      const b = videos.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
-      const len = density.length;
       const charIndex = floor(map(avg, 0, 255, 0, 29));
       const c = density.charAt(charIndex);
       if (c == ' ') asciiImage += "&nbsp;";
       else asciiImage += c;
     }
-    asciiImage += '<br class="ASCII"/>';
+    asciiImage += '<br/>';
   }
   asciiDiv.html(asciiImage);
 }
